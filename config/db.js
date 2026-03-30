9db1e40e345a6bb1ab6b4c28f262e608614f4f88
Rajratnam02 <rajratnam2005@gmail.com>
2026-03-30 18:01:14 +0530

    Updated db.js

--- config/db.js
+++ config/db.js
@@ -1,16 +1,25 @@
-import mongoose from "mongoose";
+// =================================================
+// 📌 Connect to MongoDB Database
+// =================================================
 
-const connectDb = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "recipeData", // optional
-    });
+import mongoose from "mongoose"; 
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("Database connection failed:", error.message);
-    process.exit(1);
-  }
+// --------------------
+// 🔗 MongoDB Connection Function
+// --------------------
+const connectMongoose = async () => {
+    try {
+//      ------------------Establishing connection with MongoDb------------------  
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+//      ------------------Consoling to backend------------------        
+        console.log(`🚀 Server connected with MongoDB: ${connect.connection.host}`);
+    } 
+//      ------------------Error Handling------------------    
+    catch (error) {
+        // Log error and exit the process forcefully if failed
+        console.log(`❌ MongoDB Connection Error: ${error}`);
+        process.exit(1);
+    }
 };
 
-export default connectDb;
+export default connectMongoose;
