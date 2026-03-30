b39839ff58e78263dec4666ff3d2d5e8e7ac04e9
Rajratnam02 <rajratnam2005@gmail.com>
2026-03-30 17:46:53 +0530

    Updated

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(helmet());
 
 
 app.use(cors({
-  origin: "http://localhost:5173", // change to frontend URL in prod
+  origin: "https://dish-dash-kappa.vercel.app", // change to frontend URL in prod
   credentials: true
 }));
 
